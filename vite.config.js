79a3b004896fb6b3fdcadc5d7f79d0a5d424925f
Nazarii Koropis <nazarkoropisprogramming@gmail.com>
2026-03-17 15:34:03 +0200

    change project settings and add new packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+
+      //basic
+      '@': path.resolve(__dirname, './src'),
+
+      //components
+      '@components': path.resolve(__dirname, './src/components'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@layout': path.resolve(__dirname, './src/components/layout'),
+      '@shared': path.resolve(__dirname, './src/components/shared'),
+
+      //api
+      '@api': path.resolve(__dirname, './src/api'),
+
+      //pages
+      '@pages': path.resolve(__dirname, './src/pages'),
+
+      // styles
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@images': path.resolve(__dirname, './src/assets/images')
+    }
+  }
 })
