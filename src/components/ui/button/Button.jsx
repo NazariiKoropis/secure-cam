3be92d04501629e62334ebed5cdd1fc094549920
Nazarii Koropis <nazarkoropisprogramming@gmail.com>
2026-03-17 22:09:45 +0200

    created Button Component

---
+++ src/components/ui/button/Button.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom'
+import styles from './Button.module.scss'
+import clsx from 'clsx'
+
+function Button({
+  type = 'button',
+  variant = 'primary',
+  className,
+  fullWidth = false,
+  disabled = false,
+  to,
+  href,
+  children,
+  ...props
+}) {
+  const buttonStyle = clsx(
+    styles.button,
+    styles[`button--${variant}`],
+    { [styles['button--fullwidth']]: fullWidth },
+    className,
+  )
+
+  // component Link for react-router-dom
+  if (to) {
+    return (
+      <Link to={to} className={buttonStyle} {...props}>
+        {children}
+      </Link>
+    )
+  }
+
+  // tag <a> for links like social media
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={buttonStyle}
+        target="_blank"
+        rel="noopener noreferrer"
+        {...props}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  //default button
+  return (
+    <button className={buttonStyle} type={type} disabled={disabled} {...props}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
